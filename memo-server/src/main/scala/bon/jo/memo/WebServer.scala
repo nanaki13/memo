d8749d27f5100b99786ea6dd0c97380f7cf6d571
nanaki13 <jonathan.bonnet@laposte.net>
2021-05-01 12:03:25 +0200

    core: stage almost ok

--- memo-server/src/main/scala/bon/jo/memo/WebServer.scala
+++ memo-server/src/main/scala/bon/jo/memo/WebServer.scala
@@ -59,13 +59,14 @@ object WebServer extends App with CORSHandler {
 
         val routes = concat(rService, staticFile, uiEndPoint)
 
+        val pt = getPort
 
-        val bindingFuture = Http().newServerAt("0.0.0.0", getPort).bind(routes)
-        println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-        StdIn.readLine() // let it run until user presses return
-        bindingFuture
-          .flatMap(_.unbind()) // trigger unbinding from the port
-          .onComplete(_ => system.terminate())
+        Http().newServerAt("0.0.0.0", pt).bind(routes)
+        println(s"Server online at http://localhost:$pt/")
+//        StdIn.readLine() // let it run until user presses return
+//        bindingFuture
+//          .flatMap(_.unbind()) // trigger unbinding from the port
+//          .onComplete(_ => system.terminate())
       }
     }
   }
