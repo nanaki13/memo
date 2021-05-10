501bb048347e0ecccdfff8d38cafcf22e1a287ee
EJBO2300 <jonathan.bonnet-ext@pole-emploi.fr>
2021-05-10 08:07:16 +0200

    refactor : clean

--- memo-ui/src/main/scala/bon/jo/app/EditPerso.scala
+++ memo-ui/src/main/scala/bon/jo/app/EditPerso.scala
@@ -15,16 +15,7 @@ import org.scalajs.dom.raw.{HTMLElement, HTMLOptionElement}
 import scala.collection.mutable
 import scala.util.Random
 
-object RandomName {
-  val sylab = List("bou","ba", "bi", "cla", "d", "ri", "dim", "me", "ro", "zi","na","ki","r")
 
-  def apply(): String = {
-    val r = new Random()
-    val nbSyl = r.nextInt(5)
-    val ret = (for (i <- 0 to nbSyl) yield sylab(r.nextInt(sylab.size))).mkString("")
-    ret.head.toUpper +: ret.tail
-  }
-}
 
 object EditPerso extends HtmlRep[IntBaseStat, EditPerso] {
   implicit val alg: Alg[HTMLElement] = new Alg[HTMLElement] {
---
+++ memo-ui/src/main/scala/bon/jo/app/RandomName.scala
@@ -0,0 +1,14 @@
+package bon.jo.app
+
+import scala.util.Random
+
+object RandomName {
+  val sylab = List("bou", "ba", "bi", "cla", "d", "ri", "dim", "me", "ro", "zi", "na", "ki", "r")
+
+  def apply(): String = {
+    val r = new Random()
+    val nbSyl = r.nextInt(5)
+    val ret = (for (i <- 0 to nbSyl) yield sylab(r.nextInt(sylab.size))).mkString("")
+    ret.head.toUpper +: ret.tail
+  }
+}
