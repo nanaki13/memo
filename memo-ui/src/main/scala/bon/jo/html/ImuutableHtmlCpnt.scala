c0236778998df9e3b252980968926d88114b92a7
nanaki13 <jonathan.bonnet@laposte.net>
2021-11-23 06:36:58 +0100

    refractor

--- memo-ui/src/main/scala/bon/jo/html/ImuutableHtmlCpnt.scala
+++
@@ -1,9 +0,0 @@
-package bon.jo.html
-
-import bon.jo.html.HtmlRep.HtmlCpnt
-import org.scalajs.dom.raw.HTMLElement
-
-trait ImuutableHtmlCpnt extends HtmlCpnt:
-  def create(): IterableOnce[HTMLElement]
-
-  override lazy val get: IterableOnce[HTMLElement] = create()
